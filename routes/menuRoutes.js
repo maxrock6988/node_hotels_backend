ed8ae086cd66ea5d91c570b57fd0a3d97c78dfb1
maxrock6988 <iashdasofficial@gmail.com>
2026-06-27 00:40:53 +0530

    error solving for deploy

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const router=express.Router()
 
-const MenuItem = require('./../models/menuItem');
+const MenuItem = require('../models/MenuItem');
 
 router.post('/', async (req, res) => {
     try {
