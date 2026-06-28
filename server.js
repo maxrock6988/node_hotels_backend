f5b218d21e9af95cc9189a75aa581e8f1465516c
maxrock6988 <iashdasofficial@gmail.com>
2026-06-28 14:13:23 +0530

    middleware for logging

--- server.js
+++ server.js
@@ -6,23 +6,34 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
 
+
+
+
+const logRequest=(req,res,next)=>{
+    console.log(`[${new Date().toLocaleString()} Request Made to:${req.originalUrl}]`);
+    next();
+}
+
+
+app.use(logRequest);
+
+
+
 // Home Route
-app.get('/', (req, res) => {
+app.get('/',logRequest, (req, res) => {
     res.send('Welcome to my hotel');
 });
 
-
-
 //import router
 const personRoutes = require("./routes/personRoutes");
-app.use('/person',personRoutes)
+app.use('/person', personRoutes)
 
 //import router
 const menuRoutes = require("./routes/menuRoutes");
-app.use('/menuItem',menuRoutes)
+app.use('/menuItem', menuRoutes)
 
 // Start Server
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Listening on port 3000');
